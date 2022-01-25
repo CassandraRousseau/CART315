d45e3834ee25326a383db491b220e93e5cd81ea9
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2022-01-24 22:28:28 -0500

    Exercise2: worked on exercise 2

---
+++ unity_learning/Exercise2/Assets/RotaterScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotaterScript : MonoBehaviour
+{
+    public float rotationSpeed = 0.1f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.R))
+        { this.GetComponent<Transform>().Rotate(0, rotationSpeed, 0); }
+
+        if (Input.GetKey(KeyCode.T))
+        { this.GetComponent<Transform>().Rotate(0, -rotationSpeed, 0); }
+    }
+
+    public void 
+}
